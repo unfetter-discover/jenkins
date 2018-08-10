0be310abf7247bbcdfebfab3b29fbb52cc306c64
Tavon Manning <tmann11@github.com>
2018-08-10 19:03:44 +0000

    Second upload check-in

---
+++ downloadproject.sh
@@ -0,0 +1,7 @@
+mkdir unfetter-discover
+git clone git@github.com:unfetter-discover/unfetter.git
+git clone git@github.com:unfetter-discover/unfetter-ui.git
+git clone git@github.com:unfetter-discover/unfetter-store.git
+git clone git@github.com:unfetter-discover/stix2pattern.git
+cd unfetter
+docker-compose up
