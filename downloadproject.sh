cdd4a37663974c64560166f588f1aa17617eb7c9
Tavon Manning <tmann11@github.com>
2018-08-13 20:04:47 +0000

    Update to download and jenkins scripts for docker start and script functioning.

--- downloadproject.sh
+++ downloadproject.sh
@@ -4,4 +4,5 @@ git clone git@github.com:unfetter-discover/unfetter-ui.git
 git clone git@github.com:unfetter-discover/unfetter-store.git
 git clone git@github.com:unfetter-discover/stix2pattern.git
 cd unfetter
-docker-compose up
+sudo service docker start
+docker-compose -f docker-compose.yml -f docker-compose.development.yml up
