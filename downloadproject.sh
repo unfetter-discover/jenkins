e629427388678f487f735b3df536341f7f536148
tjmanni <tjmanni@purplelabs.io>
2018-09-28 17:18:32 +0000

    downloadproject and jenkins2 upload

--- downloadproject.sh
+++ downloadproject.sh
@@ -6,4 +6,5 @@ git clone git@github.com:unfetter-discover/unfetter-store.git
 git clone git@github.com:unfetter-discover/stix2pattern.git
 cd unfetter
 sudo service docker start
-docker-compose -f docker-compose.yml -f docker-compose.development.yml up
+#docker-compose -f docker-compose.yml -f docker-compose.development.yml up
+docker-compose up 
