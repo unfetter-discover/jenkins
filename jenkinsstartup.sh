51b7274993f66507f69c28529aae2c7c22c40773
Tavon Manning <tjmanni@purplelabs.io>
2018-11-01 17:30:37 +0000

    Update to script for jenkins build

--- jenkinsstartup.sh
+++ jenkinsstartup.sh
@@ -1,5 +1,6 @@
 #Before we begin installation, we need to be in the root directory to set up libraries for Jenkins.  Type "sudo su" to gain access to the root.
 #sudo su
+sudo -i
 yum update -y
 yum install -y wget rpm java-1.8.0-openjdk-devel
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.ci.org/redhat/jenkins.repo
@@ -9,4 +10,5 @@ service jenkins start
 #Web browser needs to be launched.  Type http://your.private.ip.address:8080
 #As mentioned, enter the password found in the folder location.  The location for the password will be found at cat /var/lib/jenkins/secrets/initialAdminPassword
 #
-
+#Once password is found and copied, the Jenkins installation script will direct you to the "Customize Jenkins" page.  When there, click the blue install suggested plugins box.  
+# Select Create First Admin User and then Save and Finish.  Then, you can select Start using Jenkins.
